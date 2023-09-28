99af06ad18cd7f5433c057e90c3c0c8132685a25
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-28 01:40:30 -0700

    feat(blog): add initial layout to index page

--- src/app/pages/blog/index.page.ts
+++ src/app/pages/blog/index.page.ts
@@ -10,13 +10,20 @@ import { BlogPost } from '@models/post';
   standalone: true,
   imports: [NgFor, RouterLink, AsyncPipe],
   template: `
-    <h2>Posts</h2>
-
-    <ul>
-      <li *ngFor="let post of posts">
-        <a [routerLink]="['/blog', post.slug]">{{ post.attributes.title }}</a>
-      </li>
-    </ul>
+    <div class="md:max-w md:mx-auto md:flex md:flex-col md:items-center">
+      <div class="md:w-[48rem] p-4">
+        <div class="flex-1">
+          <h1 class="md:flex md:flex-col md:self-start">Blog Posts:</h1>
+          <ul>
+            <li *ngFor="let post of posts">
+              <a [routerLink]="['/blog', post.slug]">{{
+                post.attributes.title
+              }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   `,
 })
 export default class IndexPageComponent {
