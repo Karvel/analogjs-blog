43bb0abd46dc2fd6f9880df68046e2a09139358e
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-28 00:40:10 -0700

    feat(app): add footer (#15)
    * chore(package): install `svg-generator`, add scripts
    * feat(app): add svgs, mapped svgs
    * feat(package): install `@ngneat/svg-icon`
    * feat(footer): add and wire up initial footer
    * feat(app, styles): apply initial holy grail layout
    * feat(models): add `Navigation` interface
    * feat(config, footer): wire up social icons, svgs
    * feat(app): clean up root styling
    * feat(pages): remove scaffolded index page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import FooterComponent from '@components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <router-outlet></router-outlet> `,
-  styles: [
-    `
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
-      }
-    `,
-  ],
+  imports: [FooterComponent, RouterOutlet],
+  template: `
+    <div class="flex flex-col h-screen">
+      <main class="grow">
+        <router-outlet />
+      </main>
+      <app-footer />
+    </div>
+  `,
 })
 export class AppComponent {}
---
+++ src/assets/svg/index.ts
@@ -0,0 +1,15 @@
+import { devtoIcon } from '@svg/devto';
+import { emailIcon } from '@svg/email';
+import { flickrIcon } from '@svg/flickr';
+import { githubIcon } from '@svg/github';
+import { infoIcon } from '@svg/info';
+import { linkedInIcon } from '@svg/linked_in';
+
+export const svgIcons = [
+  devtoIcon,
+  emailIcon,
+  flickrIcon,
+  githubIcon,
+  infoIcon,
+  linkedInIcon,
+];
