0cbf00efd2347783864fc89cdd9dc6e30f095609
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-28 00:21:40 -0700

    feat(app): add svgs, mapped svgs

---
+++ src/app/svg/flickr.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+/* eslint-disable */
+export const flickrIcon = {
+    data: `<svg fill="currentColor" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"><path d="M23,0H9C4,0,0,4,0,9v14c0,5,4,9,9,9h14c5,0,9-4,9-9V9C32,4,28,0,23,0z M10,21c-2.8,0-5-2.2-5-5s2.2-5,5-5s5,2.2,5,5 S12.8,21,10,21z M22,21c-2.8,0-5-2.2-5-5s2.2-5,5-5s5,2.2,5,5S24.8,21,22,21z"/></g></svg>`,
+    name: 'flickr' as const
+};
