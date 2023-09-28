38999c134736575066abfa8b7596bd852cf1e877
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-28 00:45:39 -0700

    feat(app, header): add and wire up initial header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import FooterComponent from '@components/footer/footer.component';
+import HeaderComponent from '@components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
   template: `
     <div class="flex flex-col h-screen">
+      <app-header />
       <main class="grow">
         <router-outlet />
       </main>
