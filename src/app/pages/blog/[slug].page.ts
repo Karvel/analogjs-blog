9e3dab8bcec7234e67cd29bd282c512227c72429
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-29 00:53:21 -0700

    feat(slug, tailwind): slug page: add cover image (#42)
    * feat(slug): add and position post cover image
    * feat(tailwind): add text-shadow plugin
    * feat(slug): add text shadow to improve readability of text on image
    * feat(slug): remove absolute positioning when there is no cover image

--- src/app/pages/blog/[slug].page.ts
+++ src/app/pages/blog/[slug].page.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe, DatePipe, NgIf } from '@angular/common';
+import { AsyncPipe, DatePipe, NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { MarkdownComponent, injectContent } from '@analogjs/content';
@@ -7,36 +7,45 @@ import { BlogPost } from '@models/post';
 
 @Component({
   standalone: true,
-  imports: [AsyncPipe, DatePipe, MarkdownComponent, NgIf],
+  imports: [AsyncPipe, DatePipe, MarkdownComponent, NgClass, NgIf],
+  styleUrls: ['./[slug].page.scss'],
   template: `
     <div class="md:max-w md:mx-auto md:flex md:justify-center">
       <div *ngIf="post$ | async as post" class="md:max-w-3xl p-4">
         <div class="max-w mx-auto">
-          <div>
-            <h1 class="text-white text-xl font-bold text-shadow shadow-black">
-              {{ post.attributes.title }}
-            </h1>
-            <div
-              *ngIf="post.attributes.author"
-              class="text-white text-xs text-shadow shadow-black"
-            >
-              By: {{ post.attributes.author }}
-            </div>
-            <div class="flex justify-between items-center text-white text-xs">
-              <div class="flex text-shadow shadow-black">
-                <div *ngIf="post?.attributes?.last_updated">
-                  Updated {{ post.attributes.last_updated | date }}
-                </div>
-                <div
-                  *ngIf="
-                    post?.attributes?.date && post?.attributes?.last_updated
-                  "
-                  class="pl-2"
-                >
-                  | &nbsp;
-                </div>
-                <div *ngIf="post?.attributes?.date">
-                  Posted {{ post.attributes.date | date }}
+          <div [ngClass]="{ relative: post.attributes.cover_image }">
+            <img
+              *ngIf="post.attributes.cover_image"
+              [src]="post.attributes.cover_image"
+              [alt]="post.attributes.cover_image_title"
+              class="w-full max-w-full rounded-md"
+            />
+            <div [ngClass]="{ image_container: post.attributes.cover_image }">
+              <h1 class="text-white text-xl font-bold text-shadow shadow-black">
+                {{ post.attributes.title }}
+              </h1>
+              <div
+                *ngIf="post.attributes.author"
+                class="text-white text-xs text-shadow shadow-black"
+              >
+                By: {{ post.attributes.author }}
+              </div>
+              <div class="flex justify-between items-center text-white text-xs">
+                <div class="flex text-shadow shadow-black">
+                  <div *ngIf="post?.attributes?.last_updated">
+                    Updated {{ post.attributes.last_updated | date }}
+                  </div>
+                  <div
+                    *ngIf="
+                      post?.attributes?.date && post?.attributes?.last_updated
+                    "
+                    class="pl-2"
+                  >
+                    | &nbsp;
+                  </div>
+                  <div *ngIf="post?.attributes?.date">
+                    Posted {{ post.attributes.date | date }}
+                  </div>
                 </div>
               </div>
             </div>
