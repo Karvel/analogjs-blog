476c7da58f5c978d97a2a837126d46f4c4e1d963
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-29 00:10:18 -0700

    feat(index): add most recent three posts

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,16 +1,34 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { injectContentFiles } from '@analogjs/content';
+
+import { BlogCardComponent } from '@components/blog-card/blog-card.component';
+import { BlogPost } from '@models/post';
+import { sortByUpdatedOrOriginalDate } from '@utils/sort-by-updated-or-original-date';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [BlogCardComponent, NgFor],
   template: `
     <div class="md:max-w md:mx-auto md:flex md:justify-center">
       <div class="md:w-[48rem] p-4">
         <div class="flex-1">
           <h1>Home</h1>
+          <h2>Latest Blog Posts:</h2>
+          <ul>
+            <li *ngFor="let post of posts">
+              <app-blog-card [post]="post" />
+            </li>
+          </ul>
         </div>
       </div>
     </div>
   `,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  posts = injectContentFiles<BlogPost>()
+    .sort(sortByUpdatedOrOriginalDate)
+    .slice(0, 3);
+}
