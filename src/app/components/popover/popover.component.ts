f6004fb71d2824d78826cb82f4a37c7383f28896
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-29 01:05:20 -0700

    feat(popover): add popover component

---
+++ src/app/components/popover/popover.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-popover',
+  standalone: true,
+  template: `
+    <img
+      (click)="toggle()"
+      (keypress)="toggle()"
+      [src]="icon"
+      [alt]="altText"
+      class="cursor-pointer"
+      tabindex="0"
+      width="20"
+    />
+    <div
+      class="popover absolute right-4 bottom-12 pointer-events-none transition duration-500 ease-in-out"
+      [class.active]="isActive"
+    >
+      <ng-content />
+    </div>
+  `,
+  styleUrls: ['./popover.component.scss'],
+})
+export default class PopoverComponent {
+  @Input() public altText!: string | undefined;
+  @Input() public icon!: string | undefined;
+
+  public isActive = false;
+
+  public toggle(): void {
+    this.isActive = !this.isActive;
+  }
+}
