89787ecabc8a04a823cccd2e568a9f377c01166a
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-28 23:17:20 -0700

    feat(blog): bind `app-blog-card` to template

--- src/app/pages/blog/index.page.ts
+++ src/app/pages/blog/index.page.ts
@@ -4,11 +4,12 @@ import { RouterLink } from '@angular/router';
 
 import { injectContentFiles } from '@analogjs/content';
 
+import { BlogCardComponent } from '@components/blog-card/blog-card.component';
 import { BlogPost } from '@models/post';
 
 @Component({
   standalone: true,
-  imports: [AsyncPipe, NgFor, RouterLink],
+  imports: [AsyncPipe, BlogCardComponent, NgFor, RouterLink],
   template: `
     <div class="md:max-w md:mx-auto md:flex md:flex-col md:items-center">
       <div class="md:w-[48rem] p-4">
@@ -16,9 +17,7 @@ import { BlogPost } from '@models/post';
           <h1 class="md:flex md:flex-col md:self-start">Blog Posts:</h1>
           <ul>
             <li *ngFor="let post of posts">
-              <a [routerLink]="['/blog', post.slug]">{{
-                post.attributes.title
-              }}</a>
+              <app-blog-card [post]="post" />
             </li>
           </ul>
         </div>
