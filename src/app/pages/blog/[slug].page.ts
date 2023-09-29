f94ffedb3f2af6570898a94372f4dc6c2ff96b12
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-29 00:22:00 -0700

    feat(slug): add more front matter info to post header (#41)
    * feat(slug): add author section
    * feat(slug): show updated when available

--- src/app/pages/blog/[slug].page.ts
+++ src/app/pages/blog/[slug].page.ts
@@ -16,8 +16,25 @@ import { BlogPost } from '@models/post';
             <h1 class="text-white text-xl font-bold text-shadow shadow-black">
               {{ post.attributes.title }}
             </h1>
+            <div
+              *ngIf="post.attributes.author"
+              class="text-white text-xs text-shadow shadow-black"
+            >
+              By: {{ post.attributes.author }}
+            </div>
             <div class="flex justify-between items-center text-white text-xs">
               <div class="flex text-shadow shadow-black">
+                <div *ngIf="post?.attributes?.last_updated">
+                  Updated {{ post.attributes.last_updated | date }}
+                </div>
+                <div
+                  *ngIf="
+                    post?.attributes?.date && post?.attributes?.last_updated
+                  "
+                  class="pl-2"
+                >
+                  | &nbsp;
+                </div>
                 <div *ngIf="post?.attributes?.date">
                   Posted {{ post.attributes.date | date }}
                 </div>
