82adc7794b0d844f69b4bbd2882dcf0970d0a63f
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-30 14:21:43 -0700

    feat(metadata): add `setMetaTagsFromFrontMatter()`

--- src/app/services/metadata.service.ts
+++ src/app/services/metadata.service.ts
@@ -1,11 +1,16 @@
 import { DOCUMENT } from '@angular/common';
+import { ContentFile } from '@analogjs/content';
 import { Injectable, inject } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
+import { BlogPost } from '@models/post';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MetadataService {
   private document = inject(DOCUMENT);
+  private meta = inject(Meta);
 
   public setCanonicalUrl(url: string): void {
     const relLinkType = 'canonical';
@@ -18,5 +23,70 @@ export class MetadataService {
     }
     element.setAttribute('rel', relLinkType);
     element.setAttribute('href', url);
+  }
+
+  public setMetaTagsFromFrontMatter(
+    frontMatter: ContentFile<BlogPost | Record<string, never>>,
+  ): void {
+    if (frontMatter.attributes.title) {
+      this.meta.updateTag({
+        name: 'title',
+        content: frontMatter.attributes.title,
+      });
+      this.meta.updateTag({
+        property: 'og:title',
+        content: frontMatter.attributes.title,
+      });
+      this.meta.updateTag({
+        property: 'twitter:title',
+        content: frontMatter.attributes.title,
+      });
+    }
+    if (frontMatter.attributes.description) {
+      this.meta.updateTag({
+        name: 'description',
+        content: frontMatter.attributes.description,
+      });
+      this.meta.updateTag({
+        property: 'og:description',
+        content: frontMatter.attributes.description,
+      });
+      this.meta.updateTag({
+        property: 'twitter:description',
+        content: frontMatter.attributes.description,
+      });
+    }
+    if (frontMatter.attributes.author) {
+      this.meta.updateTag({
+        name: 'author',
+        content: frontMatter.attributes.author,
+      });
+    }
+    if (frontMatter.attributes.cover_image) {
+      this.meta.updateTag({
+        property: 'og:image',
+        content: frontMatter.attributes.cover_image,
+      });
+      this.meta.updateTag({
+        property: 'twitter:image',
+        content: frontMatter.attributes.cover_image,
+      });
+    }
+    if (frontMatter.attributes.date) {
+      this.meta.updateTag({
+        property: 'article:published_time',
+        content: frontMatter.attributes.date,
+      });
+    }
+    if (frontMatter.attributes.last_updated) {
+      this.meta.updateTag({
+        property: 'article:modified_time',
+        content: frontMatter.attributes.last_updated,
+      });
+    }
+    this.meta.updateTag({
+      property: 'twitter:card',
+      content: 'summary_large_image',
+    });
   }
 }
