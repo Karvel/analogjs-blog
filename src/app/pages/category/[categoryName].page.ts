c8e4f058f88d960d143eadfc4398d824f3527dce
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-29 17:49:01 -0700

    feat(category): filter posts by category

--- src/app/pages/category/[categoryName].page.ts
+++ src/app/pages/category/[categoryName].page.ts
@@ -1,8 +1,16 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { ContentFile, injectContentFiles } from '@analogjs/content';
+
+import { BlogCardComponent } from '@components/blog-card/blog-card.component';
+import { BlogPost } from '@models/post';
+import { sortByUpdatedOrOriginalDate } from '@utils/sort-by-updated-or-original-date';
+
 @Component({
   standalone: true,
+  imports: [BlogCardComponent, NgFor, NgIf],
   template: `
     <div class="md:max-w md:mx-auto md:flex md:flex-col md:items-center">
       <div class="md:w-[48rem] p-4">
@@ -10,6 +18,11 @@ import { ActivatedRoute } from '@angular/router';
           <h1 class="md:flex md:flex-col md:self-start">
             Category: {{ categoryName }}
           </h1>
+          <ul *ngIf="filteredPosts?.length">
+            <li *ngFor="let post of filteredPosts">
+              <app-blog-card [post]="post" />
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -17,10 +30,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export default class CategoryNamePageComponent implements OnInit {
   public categoryName!: string;
+  public filteredPosts!: ContentFile<BlogPost>[];
 
+  private posts = injectContentFiles<BlogPost>().sort(
+    sortByUpdatedOrOriginalDate,
+  );
   private route = inject(ActivatedRoute);
 
   public ngOnInit(): void {
     this.categoryName = this.route.snapshot.paramMap.get('categoryName') || '';
+    this.filteredPosts = this.filterBlogPostsByCategory(
+      this.posts,
+      this.categoryName,
+    );
+  }
+
+  private filterBlogPostsByCategory(
+    posts: ContentFile<BlogPost>[],
+    categoryToFilter: string,
+  ): ContentFile<BlogPost>[] {
+    return posts.filter(
+      (post) => post.attributes.category === categoryToFilter,
+    );
   }
 }
