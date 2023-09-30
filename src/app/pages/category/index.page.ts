fa28e7e362d7ed124d0f2d14694524bc8c313c0d
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-29 17:43:38 -0700

    feat(index): extract unique categories from post list, display as links

--- src/app/pages/category/index.page.ts
+++ src/app/pages/category/index.page.ts
@@ -1,6 +1,12 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
+import { ContentFile, injectContentFiles } from '@analogjs/content';
 import { RouteMeta } from '@analogjs/router';
+
+import { BlogPost } from '@models/post';
+import { sortByUpdatedOrOriginalDate } from '@utils/sort-by-updated-or-original-date';
 
 export const routeMeta: RouteMeta = {
   title: `Categories | Hapax Legomenon`,
@@ -8,14 +14,41 @@ export const routeMeta: RouteMeta = {
 
 @Component({
   standalone: true,
+  imports: [NgFor, RouterLink],
   template: `
     <div class="md:max-w md:mx-auto md:flex md:flex-col md:items-center">
       <div class="md:w-[48rem] p-4">
         <div class="flex-1">
           <h1 class="md:flex md:flex-col md:self-start">Categories:</h1>
+          <ul class="pt-5 flex flex-wrap justify-evenly">
+            <li *ngFor="let category of categories" class="flex m-1">
+              <a [routerLink]="['/category', category]">{{ category }}</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
   `,
 })
-export default class IndexPageComponent {}
+export default class IndexPageComponent {
+  public posts = injectContentFiles<BlogPost>().sort(
+    sortByUpdatedOrOriginalDate,
+  );
+  public categories = this.extractUniqueCategories(this.posts);
+
+  private extractUniqueCategories(
+    blogPosts: ContentFile<BlogPost>[],
+  ): string[] {
+    const uniqueCategories = new Set<string>();
+
+    for (const post of blogPosts) {
+      if (post.attributes.category) {
+        uniqueCategories.add(post.attributes.category.toLowerCase());
+      }
+    }
+
+    const uniqueCategoriesArray = Array.from(uniqueCategories);
+
+    return uniqueCategoriesArray;
+  }
+}
