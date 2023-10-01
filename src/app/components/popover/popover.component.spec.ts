292c57c899109cccf6a1e1ef80fe29063731269f
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-30 19:27:48 -0700

    style(popover): reorder imports

--- src/app/components/popover/popover.component.spec.ts
+++ src/app/components/popover/popover.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import PopoverComponent from './popover.component';
 
