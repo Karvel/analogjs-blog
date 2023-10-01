ad93f3059d51cf12b02ab624d87d4f4a9db82c40
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-30 22:08:19 -0700

    feat(metadata): add tests

--- src/app/services/metadata.service.spec.ts
+++ src/app/services/metadata.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { DOCUMENT } from '@angular/common';
 import { Meta, Title } from '@angular/platform-browser';
 
+import { ContentFile } from '@analogjs/content';
 import { vi } from 'vitest';
 
 import { metadataTags } from '@constants/metadata-tags';
+import { BlogPost } from '@models/post';
 import { MetadataService } from './metadata.service';
 
 describe('MetadataService', () => {
@@ -17,6 +20,7 @@ describe('MetadataService', () => {
     });
 
     service = TestBed.inject(MetadataService);
+    document = TestBed.inject(DOCUMENT);
     meta = TestBed.inject(Meta);
     title = TestBed.inject(Title);
   });
@@ -29,6 +33,58 @@ describe('MetadataService', () => {
     vi.spyOn(meta, 'removeTag');
     service.removeTags();
     expect(meta.removeTag).toHaveBeenCalledTimes(metadataTags.length);
+  });
+
+  it('should set canonical URL', () => {
+    const url = 'https://example.com';
+    const createElementSpy = vi.spyOn(document, 'createElement');
+    vi.spyOn(document, 'querySelector').mockReturnValue(null);
+    vi.spyOn(document.head, 'appendChild');
+    vi.spyOn(meta, 'updateTag');
+
+    service.setCanonicalUrl(url);
+
+    expect(createElementSpy).toHaveBeenCalledWith('link');
+    expect(document.head.appendChild).toHaveBeenCalled();
+    expect(meta.updateTag).toHaveBeenCalledTimes(2);
+  });
+
+  it('should set meta tags from front matter', () => {
+    const frontMatter: ContentFile<BlogPost> = {
+      attributes: {
+        description: 'Test description',
+        author: 'Test author',
+        cover_image: 'Test cover image',
+        date: '2023-09-28T00:00:00Z',
+        last_updated: '2023-09-29T00:00:00Z',
+      },
+      filename: 'test.md',
+      slug: 'test',
+    };
+
+    vi.spyOn(meta, 'updateTag');
+
+    service.setMetaTagsFromFrontMatter(frontMatter);
+
+    expect(meta.updateTag).toHaveBeenCalledTimes(9);
+  });
+
+  it('should set page URL meta tags', () => {
+    const pageUrl = 'https://example.com';
+    vi.spyOn(meta, 'updateTag');
+
+    service.setPageURLMetaTags(pageUrl);
+
+    expect(meta.updateTag).toHaveBeenCalledTimes(2);
+  });
+
+  it('should set page URL meta title', () => {
+    const titleValue = 'Test Title';
+    vi.spyOn(meta, 'updateTag');
+
+    service.setPageURLMetaTitle(titleValue);
+
+    expect(meta.updateTag).toHaveBeenCalledTimes(3);
   });
 
   it('should set title', () => {
