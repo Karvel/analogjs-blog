1426d5ff421bf4242f9961d70814a1b557547753
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-30 17:45:28 -0700

    feat(app): host fallback image locally

--- src/app/constants/fallback-image-path.ts
+++ src/app/constants/fallback-image-path.ts
@@ -1,3 +1,2 @@
 // TODO: Replace with site placeholder image
-export const fallbackImagePath =
-  'https://live.staticflickr.com/2458/3776870085_fe285e9698_z.jpg';
+export const fallbackImagePath = 'fallback_cover_image.jpg';
