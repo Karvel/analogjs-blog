7c3cb4ada3b2a31e250e17cf4b84e94120827abf
Elanna Grossman <elanna.grossman@gmail.com>
2023-09-30 17:27:34 -0700

    feat(directives): add `ReplaceBrokenImageDirective`

---
+++ src/app/directives/replace-broken-image.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, HostListener, inject } from '@angular/core';
+
+import { fallbackImagePath } from '@constants/fallback-image-path';
+import { siteName } from '@constants/site-name';
+
+@Directive({
+  selector: 'img[appReplaceBrokenImage]',
+  standalone: true,
+})
+export class ReplaceBrokenImageDirective {
+  private el = inject(ElementRef);
+
+  @HostListener('error')
+  public onError(): void {
+    this.el.nativeElement.src = `${fallbackImagePath}`;
+    this.el.nativeElement.alt = `${siteName}`;
+  }
+}
