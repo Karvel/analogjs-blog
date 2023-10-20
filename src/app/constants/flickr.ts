a8df300a1d7fd6761a6bd1e166aa4c2f49742eb9
Elanna Grossman <elanna.grossman@gmail.com>
2023-10-19 21:27:37 -0700

    feat(constants): add flickr values

---
+++ src/app/constants/flickr.ts
@@ -0,0 +1,6 @@
+export const flickr = {
+  api_key: 'OGU2NGZlMDBkMjEyN2ExNzM0ZWUyOTg5ZmI5MGM1ODc=',
+  extras:
+    'date_upload, date_taken, owner_name, last_update, machine_tags, o_dims, path_alias, url_sq, url_t, url_s, url_m, url_o',
+  user_id: '83957127@N00',
+};
