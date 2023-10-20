0dae95ac70399d17d882ec4c452d0a960aea8adf
Elanna Grossman <elanna.grossman@gmail.com>
2023-10-19 21:51:55 -0700

    feat(photo-album): add link to album

--- src/app/components/photo-album/photo-album.component.ts
+++ src/app/components/photo-album/photo-album.component.ts
@@ -8,41 +8,47 @@ import { ReplaceBrokenImageDirective } from '@directives/replace-broken-image.di
   standalone: true,
   imports: [NgIf, ReplaceBrokenImageDirective],
   template: `
-    <div class="relative">
-      <img
-        *ngIf="photo.id"
-        [src]="
-          flickrUrl +
-          '/' +
-          photo.server +
-          '/' +
-          photo.primary +
-          '_' +
-          photo.secret +
-          '_w.jpg'
-        "
-        [alt]="photo.title._content"
-        appReplaceBrokenImage
-        class="rounded-md"
-      />
-      <div
-        class="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-end p-4"
+    <div *ngIf="photo.id" class="relative">
+      <a
+        [href]="flickrAlbumUrl + '/' + photo.id"
+        target="_blank"
+        rel="noopener"
       >
-        <h1 class="text-white text-xl font-bold text-shadow shadow-black">
-          {{ photo.title._content }}
-        </h1>
+        <img
+          [src]="
+            flickrAlbumPhotoUrl +
+            '/' +
+            photo.server +
+            '/' +
+            photo.primary +
+            '_' +
+            photo.secret +
+            '_w.jpg'
+          "
+          [alt]="photo.title._content"
+          appReplaceBrokenImage
+          class="rounded-md"
+        />
         <div
-          *ngIf="photo.username"
-          class="text-white text-xs text-shadow shadow-black"
+          class="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-end p-4"
         >
-          By: {{ photo.username }}
+          <h1 class="text-white text-xl font-bold text-shadow shadow-black">
+            {{ photo.title._content }}
+          </h1>
+          <div
+            *ngIf="photo.username"
+            class="text-white text-xs text-shadow shadow-black"
+          >
+            By: {{ photo.username }}
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   `,
 })
 export class PhotoAlbumComponent {
   @Input() public photo: any;
 
-  public flickrUrl = 'https://live.staticflickr.com';
+  public flickrAlbumPhotoUrl = 'https://live.staticflickr.com';
+  public flickrAlbumUrl = 'https://www.flickr.com/photos/jadeilyn/albums';
 }
