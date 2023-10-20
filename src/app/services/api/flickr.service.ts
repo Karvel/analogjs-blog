a6db8663622e10518ac80997d5db71e9b164c38f
Elanna Grossman <elanna.grossman@gmail.com>
2023-10-20 00:14:08 -0700

    feat(services): add `getFavoritePhotos()`

--- src/app/services/api/flickr.service.ts
+++ src/app/services/api/flickr.service.ts
@@ -5,6 +5,7 @@ import { Observable, map } from 'rxjs';
 
 import { flickr } from '@constants/flickr';
 import { PhotoSetListItem, PhotoSetsListResponse } from '@models/flickr';
+import { shuffleArray } from '@utils/shuffle-array';
 import { ApiService } from './api.service';
 
 @Injectable({
@@ -13,6 +14,28 @@ import { ApiService } from './api.service';
 export class FlickrService {
   private apiService = inject(ApiService);
   private baseUrl = 'https://www.flickr.com/services/rest';
+
+  /**
+   * Get 9 randomized photos from my photo set of favorite photos.
+   */
+  public getFavoritePhotos(): Observable<any> {
+    const paramObj = {
+      method: 'flickr.photosets.getPhotos',
+      api_key: atob(flickr.api_key),
+      user_id: flickr.user_id,
+      photoset_id: '72157604124791010',
+      extras: flickr.extras,
+      format: 'json',
+      nojsoncallback: '1',
+    };
+    const params = new HttpParams({ fromObject: paramObj });
+
+    return this.apiService
+      .get<any>(`${this.baseUrl}`, { params })
+      .pipe(
+        map((response) => shuffleArray(response.photoset.photo).slice(0, 9)),
+      );
+  }
 
   /**
    * Get 3 most recent photo sets, excluding the first, which is a collection of favorites.
