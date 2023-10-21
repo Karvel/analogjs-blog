8c425019395d3c7a28e5d4d89c4c9cd8290c4212
Elanna Grossman <elanna.grossman@gmail.com>
2023-10-20 22:30:41 -0700

    chore(routes): add initial RSS implementation

---
+++ src/server/routes/rss.xml.ts
@@ -0,0 +1,43 @@
+import { defineEventHandler } from 'h3';
+import RSS from 'rss';
+import * as fs from 'fs';
+import * as path from 'path';
+import fm from 'front-matter';
+const posts = fs.readdirSync('./src/content');
+async function generateRssFeed() {
+  const site_url = 'https://elanna.me';
+
+  const feedOptions = {
+    title: 'Hapax Legomenon | RSS Feed',
+    description: 'Hapax Legomenon Blog Posts',
+    site_url: site_url,
+    feed_url: `${site_url}/api/rss.xml`,
+    pubDate: new Date(),
+    copyright: `All rights reserved ${new Date().getFullYear()}`,
+  };
+
+  const feed = new RSS(feedOptions);
+
+  posts.forEach((postFile) => {
+    const fileContents = fs.readFileSync(
+      path.resolve('src/content', postFile),
+      'utf8',
+    );
+    const post: any = fm(fileContents).attributes;
+
+    feed.item({
+      title: post.title,
+      description: post.description,
+      url: `${site_url}/blog/${post.slug}`,
+      date: post.publishedDate,
+    });
+  });
+
+  return feed.xml({ indent: true });
+}
+
+export default defineEventHandler(async (event) => {
+  const feedString = await generateRssFeed();
+  event.node.res.setHeader('content-type', 'text/xml');
+  event.node.res.end(feedString);
+});
