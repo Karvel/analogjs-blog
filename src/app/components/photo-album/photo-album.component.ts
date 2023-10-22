89958c66c9928e8e4cdfe3326e6c452fa605b105
Elanna Grossman <elanna.grossman@gmail.com>
2023-10-21 19:10:55 -0700

    feat(components, pages): tweak text shadow

--- src/app/components/photo-album/photo-album.component.ts
+++ src/app/components/photo-album/photo-album.component.ts
@@ -34,12 +34,12 @@ import { PhotosetListItem } from '@models/flickr';
         <div
           class="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-end p-4"
         >
-          <h1 class="text-white text-xl font-bold text-shadow shadow-black">
+          <h1 class="text-white text-xl font-bold text-shadow-sm shadow-black">
             {{ photo.title._content }}
           </h1>
           <div
             *ngIf="photo.username"
-            class="text-white text-xs text-shadow shadow-black"
+            class="text-white text-xs text-shadow-sm shadow-black"
           >
             By: {{ photo.username }}
           </div>
