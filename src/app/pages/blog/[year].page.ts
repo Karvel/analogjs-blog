4d5b732902fc38892892d1fa6b9c65846fc0f58a
Elanna Grossman <elanna.grossman@gmail.com>
2023-10-24 18:40:22 -0700

    feat(blog): add `[year]` route component

---
+++ src/app/pages/blog/[year].page.ts
@@ -0,0 +1,104 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { MetaDefinition } from '@angular/platform-browser';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import { ContentFile, injectContentFiles } from '@analogjs/content';
+
+import { BlogCardComponent } from '@components/blog-card/blog-card.component';
+import { siteName } from '@constants/site-name';
+import { BlogPost } from '@models/post';
+import { MetadataService } from '@services/metadata.service';
+import { sortByUpdatedOrOriginalDate } from '@utils/sort-by-updated-or-original-date';
+
+@Component({
+  selector: 'app-year-page',
+  standalone: true,
+  imports: [BlogCardComponent, NgFor, NgIf, RouterLink],
+  template: `
+    <div
+      class="md:max-w md:mx-auto md:flex md:flex-col md:items-center text-xl"
+    >
+      <div class="md:w-[48rem] p-4">
+        <div class="flex-1">
+          <h1 class="md:flex md:flex-col md:self-start">Year: {{ year }}</h1>
+          <ul *ngIf="filteredPosts?.length; else emptyResult">
+            <li *ngFor="let post of filteredPosts">
+              <app-blog-card [post]="post" />
+            </li>
+          </ul>
+        </div>
+        <ng-template #emptyResult
+          >There are no posts matching "{{ year }}".</ng-template
+        >
+      </div>
+    </div>
+  `,
+})
+export default class YearPageComponent implements OnInit {
+  public year!: string;
+  public filteredPosts!: ContentFile<BlogPost>[];
+
+  private metadataService = inject(MetadataService);
+  private metaTagList: MetaDefinition[] = [
+    {
+      name: 'description',
+      content: '',
+    },
+    {
+      name: 'author',
+      content: 'Elanna Grossman',
+    },
+    {
+      property: 'og:description',
+      content: '',
+    },
+    {
+      property: 'twitter:description',
+      content: '',
+    },
+  ];
+  private posts = injectContentFiles<BlogPost>((mdFile) =>
+    mdFile.filename.includes('/src/content/posts'),
+  ).sort(sortByUpdatedOrOriginalDate);
+  private route = inject(ActivatedRoute);
+
+  public ngOnInit(): void {
+    this.year = this.route.snapshot.paramMap.get('year') || '';
+    this.setPageTitle(this.year);
+    this.setMetadata(this.year);
+    this.filteredPosts = this.filterBlogPostsByYear(this.posts, this.year);
+  }
+
+  private filterBlogPostsByYear(
+    posts: ContentFile<BlogPost>[],
+    filterBy: string,
+  ): ContentFile<BlogPost>[] {
+    return posts.filter((post) => {
+      return (
+        new Date(post.attributes.date || '').getFullYear()?.toString() ===
+        filterBy
+      );
+    });
+  }
+
+  private setMetadata(year: string): void {
+    const description = `Blog posts filtered by ${year}.`;
+    this.metaTagList.map((metaTag) => {
+      metaTag.content = description;
+
+      return metaTag;
+    });
+
+    this.metadataService.updateTags(this.metaTagList);
+  }
+
+  /**
+   * Setting dynamic page title in component
+   */
+  private setPageTitle(year: string): void {
+    const title = year ? `${year} Year | ${siteName}` : `Year | ${siteName}`;
+    this.metadataService.setTitle(title);
+    this.metadataService.setPageURLMetaTitle(title);
+  }
+}
