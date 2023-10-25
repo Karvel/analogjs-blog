1fc0ede39aed2fe662c39a733b15d4435686e5ea
Elanna Grossman <elanna.grossman@gmail.com>
2023-10-24 20:39:57 -0700

    feat(slug): add archives to non-matching routes, clean up styling

--- src/app/pages/blog/[year].[month].[slug].page.ts
+++ src/app/pages/blog/[year].[month].[slug].page.ts
@@ -9,7 +9,9 @@ import {
   injectContentFiles,
   MarkdownComponent,
 } from '@analogjs/content';
+import { filter } from 'rxjs';
 
+import { ArchiveComponent } from '@components/archive/archive.component';
 import PillComponent from '@components/pill/pill.component';
 import ImageInfoPopoverContentComponent from '@components/popover/image-info-popover-content.component';
 import PopoverComponent from '@components/popover/popover.component';
@@ -20,12 +22,12 @@ import { Tag } from '@models/tag';
 import { MetadataService } from '@services/metadata.service';
 import { sortByUpdatedOrOriginalDate } from '@utils/sort-by-updated-or-original-date';
 import { splitTagStringIntoArray } from '@utils/split-tag-string-into-array';
-import { filter } from 'rxjs';
 
 @Component({
-  selector: 'app-test-slug',
+  selector: 'app-blog-slug',
   standalone: true,
   imports: [
+    ArchiveComponent,
     AsyncPipe,
     DatePipe,
     ImageInfoPopoverContentComponent,
@@ -41,129 +43,147 @@ import { filter } from 'rxjs';
   styleUrls: ['./[year].[month].[slug].page.css'],
   template: `
     <div class="md:max-w md:mx-auto md:flex md:justify-center">
-      <div *ngIf="post$ | async as post" class="md:w-[48rem] p-4">
-        <div class="max-w mx-auto">
-          <div [ngClass]="{ relative: post.attributes.cover_image }">
-            <img
-              *ngIf="post.attributes.cover_image"
-              appReplaceBrokenImage
-              [src]="post.attributes.cover_image"
-              [alt]="post.attributes.cover_image_title"
-              class="w-full max-w-full rounded-md"
-            />
-            <div [ngClass]="{ image_container: post.attributes.cover_image }">
-              <h1
-                class="text-white text-xl font-bold text-shadow-sm shadow-black"
-              >
-                {{ post.attributes.title }}
-              </h1>
-              <div
-                *ngIf="post.attributes.author"
-                class="text-white text-xs text-shadow-sm shadow-black"
-              >
-                By: {{ post.attributes.author }}
-              </div>
-              <div class="flex justify-between items-center text-white text-xs">
-                <div class="flex text-shadow-sm shadow-black">
-                  <div *ngIf="post?.attributes?.last_updated">
-                    Updated {{ post.attributes.last_updated | date }}
+      <div class="md:w-[48rem] p-4">
+        <div *ngIf="post$ | async as post; else emptyResult" class="flex-1">
+          <div class="max-w mx-auto">
+            <div [ngClass]="{ relative: post.attributes.cover_image }">
+              <img
+                *ngIf="post.attributes.cover_image"
+                appReplaceBrokenImage
+                [src]="post.attributes.cover_image"
+                [alt]="post.attributes.cover_image_title"
+                class="w-full max-w-full rounded-md"
+              />
+              <div [ngClass]="{ image_container: post.attributes.cover_image }">
+                <h1
+                  class="text-white text-xl font-bold text-shadow-sm shadow-black"
+                >
+                  {{ post.attributes.title }}
+                </h1>
+                <div
+                  *ngIf="post.attributes.author"
+                  class="text-white text-xs text-shadow-sm shadow-black"
+                >
+                  By: {{ post.attributes.author }}
+                </div>
+                <div
+                  class="flex justify-between items-center text-white text-xs"
+                >
+                  <div class="flex text-shadow-sm shadow-black">
+                    <div *ngIf="post?.attributes?.last_updated">
+                      Updated {{ post.attributes.last_updated | date }}
+                    </div>
+                    <div
+                      *ngIf="
+                        post?.attributes?.date && post?.attributes?.last_updated
+                      "
+                      class="pl-2"
+                    >
+                      | &nbsp;
+                    </div>
+                    <div *ngIf="post?.attributes?.date">
+                      Posted {{ post.attributes.date | date }}
+                    </div>
                   </div>
                   <div
                     *ngIf="
-                      post?.attributes?.date && post?.attributes?.last_updated
+                      post.attributes.cover_image &&
+                      post?.attributes?.cover_image_source &&
+                      post?.attributes?.cover_image_title
                     "
-                    class="pl-2"
+                    class="flex"
                   >
-                    | &nbsp;
+                    <app-popover
+                      [altText]="'Image information'"
+                      [icon]="'svg/info.svg'"
+                    >
+                      <app-image-info-popover-content
+                        [cover_image_author]="
+                          post?.attributes?.cover_image_author
+                        "
+                        [cover_image_source]="
+                          post.attributes.cover_image_source
+                        "
+                        [cover_image_title]="
+                          post?.attributes?.cover_image_title
+                        "
+                      />
+                    </app-popover>
                   </div>
-                  <div *ngIf="post?.attributes?.date">
-                    Posted {{ post.attributes.date | date }}
-                  </div>
-                </div>
-                <div
-                  *ngIf="
-                    post.attributes.cover_image &&
-                    post?.attributes?.cover_image_source &&
-                    post?.attributes?.cover_image_title
-                  "
-                  class="flex"
-                >
-                  <app-popover
-                    [altText]="'Image information'"
-                    [icon]="'svg/info.svg'"
-                  >
-                    <app-image-info-popover-content
-                      [cover_image_author]="
-                        post?.attributes?.cover_image_author
-                      "
-                      [cover_image_source]="post.attributes.cover_image_source"
-                      [cover_image_title]="post?.attributes?.cover_image_title"
-                    />
-                  </app-popover>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <analog-markdown
-          class="prose dark:prose-invert prose-code:before:hidden prose-code:after:hidden"
-          [content]="post.content"
-        />
-        <div class="flex flex-col">
-          <div *ngIf="post.attributes.category" class="text-sm">
-            Category:
-            <app-pill
-              [label]="post.attributes.category"
-              [route]="'/category'"
-              [slug]="post.attributes.category"
-              class="m-1"
-            />
-          </div>
-          <div *ngIf="post.attributes.tags?.length" class="text-sm">
-            Tags:
-            <ng-container
-              *ngFor="let tag of splitTagStringIntoArray(post.attributes.tags)"
-            >
+          <analog-markdown
+            class="prose dark:prose-invert prose-code:before:hidden prose-code:after:hidden"
+            [content]="post.content"
+          />
+          <div class="flex flex-col">
+            <div *ngIf="post.attributes.category" class="text-sm">
+              Category:
               <app-pill
-                [label]="tag.name"
-                [route]="'/tag'"
-                [slug]="tag.name"
-                class="mx-1"
+                [label]="post.attributes.category"
+                [route]="'/category'"
+                [slug]="post.attributes.category"
+                class="m-1"
               />
-            </ng-container>
+            </div>
+            <div *ngIf="post.attributes.tags?.length" class="text-sm">
+              Tags:
+              <ng-container
+                *ngFor="
+                  let tag of splitTagStringIntoArray(post.attributes.tags)
+                "
+              >
+                <app-pill
+                  [label]="tag.name"
+                  [route]="'/tag'"
+                  [slug]="tag.name"
+                  class="mx-1"
+                />
+              </ng-container>
+            </div>
           </div>
-        </div>
-        <div class="flex justify-between text-sm mt-2 gap-2">
-          <button
-            *ngIf="prevPost"
-            [routerLink]="['/blog', prevPost.slug]"
-            attr.alt="Click to go to the previous post: {{
-              prevPost.attributes.title
-            }}"
-            type="button"
-            class="inline-flex focus:outline-none bg-indigo-200 hover:bg-indigo-300 focus:ring-2 focus:ring-indigo-300 font-medium rounded-lg text-sm px-3 py-1 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 truncate"
-          >
-            <span>&laquo; &nbsp;</span
-            ><span class="truncate">{{ prevPost.attributes.title }}</span>
-          </button>
-          <button
-            *ngIf="nextPost"
-            [routerLink]="['/blog', nextPost.slug]"
-            attr.alt="Click to go to the next post: {{
-              nextPost.attributes.title
-            }}"
-            type="button"
-            class="inline-flex ml-auto focus:outline-none bg-indigo-200 hover:bg-indigo-300 focus:ring-2 focus:ring-indigo-300 font-medium rounded-lg text-sm px-3 py-1 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 truncate"
-          >
-            <span class="truncate">{{ nextPost.attributes.title }}</span
-            ><span>&nbsp; &raquo;</span>
-          </button>
+          <div class="flex justify-between text-sm mt-2 gap-2">
+            <button
+              *ngIf="prevPost"
+              [routerLink]="['/blog', prevPost.slug]"
+              attr.alt="Click to go to the previous post: {{
+                prevPost.attributes.title
+              }}"
+              type="button"
+              class="inline-flex focus:outline-none bg-indigo-200 hover:bg-indigo-300 focus:ring-2 focus:ring-indigo-300 font-medium rounded-lg text-sm px-3 py-1 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 truncate"
+            >
+              <span>&laquo; &nbsp;</span
+              ><span class="truncate">{{ prevPost.attributes.title }}</span>
+            </button>
+            <button
+              *ngIf="nextPost"
+              [routerLink]="['/blog', nextPost.slug]"
+              attr.alt="Click to go to the next post: {{
+                nextPost.attributes.title
+              }}"
+              type="button"
+              class="inline-flex ml-auto focus:outline-none bg-indigo-200 hover:bg-indigo-300 focus:ring-2 focus:ring-indigo-300 font-medium rounded-lg text-sm px-3 py-1 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 truncate"
+            >
+              <span class="truncate">{{ nextPost.attributes.title }}</span
+              ><span>&nbsp; &raquo;</span>
+            </button>
+          </div>
         </div>
       </div>
+      <ng-template #emptyResult>
+        <div>
+          <div class="pt-5">There is no matching post.</div>
+          <div class="mt-5">
+            <app-archive [posts]="posts" />
+          </div>
+        </div>
+      </ng-template>
     </div>
   `,
 })
-export default class TestPostPageComponent {
+export default class BlogPostPageComponent {
   private route = inject(ActivatedRoute);
   public nextPost!: ContentFile<BlogPost>;
   public post$ = injectContent<BlogPost>({
@@ -180,8 +200,8 @@ export default class TestPostPageComponent {
       );
     }),
   );
-  private posts = injectContentFiles<BlogPost>((mdFile) =>
-    mdFile.filename.includes('/src/content/test'),
+  public posts = injectContentFiles<BlogPost>((mdFile) =>
+    mdFile.filename.includes('/src/content/posts'),
   ).sort(sortByUpdatedOrOriginalDate);
   public prevPost!: ContentFile<BlogPost>;
   public splitTagStringIntoArray = splitTagStringIntoArray;
