179e0444da5696c4200ef17af0d4b5570ab60545
Elanna Grossman <elanna.grossman@gmail.com>
2023-11-20 16:16:32 -0800

    fix(app): use static site url (#127)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,6 @@ export class AppComponent implements OnInit {
   private metadataService = inject(MetadataService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  private url = 'https://elanna.me';
 
   public ngOnInit(): void {
     this.setMetaOnRouteLoad();
