12bffff325ddb586418ea31227d940e861420430
Elanna Grossman <elanna.grossman@gmail.com>
2023-12-28 17:41:26 -0800

    feat(paginator): add page size control

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -7,66 +7,81 @@ import {
   Output,
   inject,
 } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-paginator',
   standalone: true,
-  imports: [NgFor, NgIf, RouterLink],
+  imports: [NgFor, NgIf, RouterLink, ReactiveFormsModule],
   template: `
     <nav
       aria-label="Page navigation"
       class="flex items-center justify-center gap-3 mb-3"
     >
-      <a
-        class="inline-flex h-8 w-8 items-center justify-center cursor-pointer rounded border border-gray-300 bg-white text-neutral-900 dark:border-neutral-400 dark:bg-neutral-900 dark:text-white rtl:rotate-180"
-        [routerLink]="'/blog'"
-        [queryParams]="{ page: previousPage() }"
-        queryParamsHandling="merge"
-        tabindex="0"
-      >
-        <span class="sr-only">Next Page</span>
-        <svg
-          class="h-3 w-3"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
+      <div class="flex items-center justify-center gap-3 mb-3">
+        <a
+          class="inline-flex h-8 w-8 items-center justify-center cursor-pointer rounded border border-gray-300 bg-white text-neutral-900 dark:border-neutral-400 dark:bg-neutral-900 dark:text-white rtl:rotate-180"
+          [routerLink]="'/blog'"
+          [queryParams]="{ page: previousPage() }"
+          queryParamsHandling="merge"
+          tabindex="0"
         >
-          <path
-            fill-rule="evenodd"
-            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </a>
+          <span class="sr-only">Next Page</span>
+          <svg
+            class="h-3 w-3"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
 
-      <p class="text-xs">
-        {{ currentPage }}
-        <span class="mx-0.25">/</span>
-        {{ totalPages }}
-      </p>
+        <p class="text-xs">
+          {{ currentPage }}
+          <span class="mx-0.25">/</span>
+          {{ totalPages }}
+        </p>
 
-      <a
-        class="inline-flex h-8 w-8 items-center justify-center cursor-pointer rounded border border-gray-300 bg-white text-neutral-900 dark:border-neutral-400 dark:bg-neutral-900 dark:text-white rtl:rotate-180"
-        [routerLink]="'/blog'"
-        [queryParams]="{ page: nextPage() }"
-        queryParamsHandling="merge"
-        tabindex="0"
-      >
-        <span class="sr-only">Next Page</span>
-        <svg
-          class="h-3 w-3"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
+        <a
+          class="inline-flex h-8 w-8 items-center justify-center cursor-pointer rounded border border-gray-300 bg-white text-neutral-900 dark:border-neutral-400 dark:bg-neutral-900 dark:text-white rtl:rotate-180"
+          [routerLink]="'/blog'"
+          [queryParams]="{ page: nextPage() }"
+          queryParamsHandling="merge"
+          tabindex="0"
         >
-          <path
-            fill-rule="evenodd"
-            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </a>
+          <span class="sr-only">Next Page</span>
+          <svg
+            class="h-3 w-3"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </div>
+      <select
+        *ngIf="pageSizeControl"
+        [formControl]="pageSizeControl"
+        aria-label="Page Size Selector"
+        class="mb-3 border border-gray-300 bg-white text-neutral-900 text-sm rounded block p-[.375rem] dark:bg-neutral-900
+        dark:border-neutral-400 dark:text-white"
+      >
+        <option [value]="5">5</option>
+        <option [value]="10">10</option>
+        <option [value]="20">20</option>
+        <option [value]="50">50</option>
+      </select>
     </nav>
   `,
 })
@@ -75,8 +90,10 @@ export class PaginatorComponent implements OnInit {
   @Input() totalItems = 0;
 
   @Output() pageChanged = new EventEmitter<number>();
+  @Output() pageSizeChanged = new EventEmitter<number>();
 
   public currentPage = 1;
+  public pageSizeControl!: FormControl;
 
   private route = inject(ActivatedRoute);
   private router = inject(Router);
@@ -87,6 +104,7 @@ export class PaginatorComponent implements OnInit {
 
   ngOnInit() {
     this.addRouteListener();
+    this.addPageSizeListener();
   }
 
   public changePage(page: number): void {
@@ -118,6 +136,15 @@ export class PaginatorComponent implements OnInit {
       } else {
         this.changePage(1);
       }
+    });
+  }
+
+  private addPageSizeListener(): void {
+    this.pageSizeControl = new FormControl(this.itemsPerPage);
+    this.pageSizeControl.valueChanges.subscribe((newSize) => {
+      this.itemsPerPage = newSize;
+      this.pageSizeChanged.emit(this.itemsPerPage);
+      this.changePage(1); // Reset to page 1 whenever page size changes
     });
   }
 }
