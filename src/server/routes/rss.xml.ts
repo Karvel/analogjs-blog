26914671a0d70f7903da11656568e50d816ea673
Elanna Grossman <elanna.grossman@gmail.com>
2023-12-30 15:26:07 -0800

    feat(rss): add body as description

--- src/server/routes/rss.xml.ts
+++ src/server/routes/rss.xml.ts
@@ -3,6 +3,7 @@ import RSS from 'rss';
 import * as fs from 'fs';
 import * as path from 'path';
 import fm from 'front-matter';
+import { marked } from 'marked';
 
 import { BlogPost } from '@models/post';
 import { getMonth } from '../../app/utils/get-month';
@@ -32,6 +33,7 @@ async function generateRssFeed() {
       );
       return {
         attributes: fm(fileContents).attributes as BlogPost,
+        body: fm(fileContents).body,
       };
     })
     .filter(({ attributes }) => attributes.published)
@@ -39,10 +41,13 @@ async function generateRssFeed() {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (a1.attributes as any).date > (a2.attributes as any).date ? -1 : 1,
     )
-    .forEach(({ attributes }) => {
+    .forEach(({ attributes, body }) => {
+      const html = marked.parse(
+        body.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ''),
+      );
       const month = getMonth(attributes.date);
       const year = getYear(attributes.date);
-      const description = attributes.description ?? '';
+      const description = html ?? '';
       const imageMarkup = attributes.cover_image
         ? `<img src="${attributes.cover_image}" /><br />`
         : `<img src="${site_url}/images/self/fallback_cover_image.png" /><br />`;
