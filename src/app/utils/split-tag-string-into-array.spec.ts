48c05685a593e05be80da89aec70c0e916084fed
Elanna Grossman <elanna.grossman@gmail.com>
2023-12-30 15:13:08 -0800

    feat, test(): add `splitTagStringIntoStringArray()`

--- src/app/utils/split-tag-string-into-array.spec.ts
+++ src/app/utils/split-tag-string-into-array.spec.ts
@@ -1,5 +1,6 @@
 import { Tag } from '@models/tag';
 import {
+  splitTagStringIntoStringArray,
   splitTagStringIntoTagArray,
 } from './split-tag-string-into-array'; // Import the function and interface you want to test
 
@@ -64,6 +65,60 @@ describe('splitTagStringIntoTagArray', () => {
       { name: 'tag2' },
       { name: 'tag3' },
     ];
+
+    expect(result).toEqual(expectedTags); // Expect leading and trailing commas to be ignored
+  });
+});
+
+describe('splitTagStringIntoStringArray', () => {
+  it('should return an empty array when tagString is undefined', () => {
+    const result = splitTagStringIntoStringArray(undefined);
+    expect(result).toEqual([]); // Expect an empty array to be returned
+  });
+
+  it('should return an empty array when tagString is null', () => {
+    const result = splitTagStringIntoStringArray(null);
+    expect(result).toEqual([]); // Expect an empty array to be returned
+  });
+
+  it('should split a comma-separated tagString into an array of Tag objects', () => {
+    const tagString = 'tag1, tag2, tag3';
+    const result = splitTagStringIntoStringArray(tagString);
+
+    const expectedTags: string[] = ['tag1', 'tag2', 'tag3'];
+
+    expect(result).toEqual(expectedTags); // Expect an array of strings
+  });
+
+  it('should trim whitespace from tag names', () => {
+    const tagString = ' tag1,tag2 , tag3 ';
+    const result = splitTagStringIntoStringArray(tagString);
+
+    const expectedTags: string[] = ['tag1', 'tag2', 'tag3'];
+
+    expect(result).toEqual(expectedTags); // Expect tag names to be trimmed
+  });
+
+  it('should handle an empty tagString and return an empty array', () => {
+    const tagString = '';
+    const result = splitTagStringIntoStringArray(tagString);
+    expect(result).toEqual([]); // Expect an empty array to be returned
+  });
+
+  it('should handle a single tag without commas', () => {
+    const tagString = 'tag1';
+    const result = splitTagStringIntoStringArray(tagString);
+
+    const expectedTags: string[] = ['tag1'];
+
+    expect(result).toEqual(expectedTags); // Expect an array with a single string
+  });
+
+  it('should handle leading and trailing commas', () => {
+    const tagString = ',tag1,tag2,tag3,';
+    const result = splitTagStringIntoStringArray(tagString);
+
+    const expectedTags: string[] = ['tag1', 'tag2', 'tag3'];
 
     expect(result).toEqual(expectedTags); // Expect leading and trailing commas to be ignored
   });
--- src/app/utils/split-tag-string-into-array.ts
+++ src/app/utils/split-tag-string-into-array.ts
@@ -15,3 +15,18 @@ export const splitTagStringIntoTagArray = (
 
   return tagArray;
 };
+
+export const splitTagStringIntoStringArray = (
+  tagString: string | undefined | null,
+): string[] => {
+  if (tagString === undefined || tagString === null) {
+    return [];
+  }
+
+  const tagNameArray = tagString.split(',');
+  const tagArray = tagNameArray
+    .map((tagName) => tagName.trim())
+    .filter((tagName) => tagName !== ''); // Filter out empty tag names
+
+  return tagArray;
+};
