00343bc7d09418e6b32e865a9e4e8f9e3095f313
Elanna Grossman <elanna.grossman@gmail.com>
2024-01-21 19:46:07 -0800

    fix(slug): fix pagination index

--- src/app/pages/blog/[year].[month].[slug].page.ts
+++ src/app/pages/blog/[year].[month].[slug].page.ts
@@ -226,14 +226,16 @@ export default class BlogPostPageComponent {
     post: ContentFile<BlogPost | Record<string, never>>,
     posts: ContentFile<BlogPost>[],
   ): void {
+    const reversedPosts = [...posts].reverse();
     const index = posts.findIndex((p) => p.slug === post.slug);
-    const nextPublishedIndex = posts.findIndex(
-      (post, i) => i < index && post.attributes.published,
-    );
+    const reversedIndex = reversedPosts.findIndex((p) => p.slug === post.slug);
+    const nextPublishedIndex = reversedPosts.findIndex((post, i) => {
+      return i > reversedIndex && post.attributes.published;
+    });
     const previousPublishedIndex = posts.findIndex(
       (post, i) => i > index && post.attributes.published,
     );
-    const nextPost = posts[nextPublishedIndex];
+    const nextPost = reversedPosts[nextPublishedIndex];
     const previousPost = posts[previousPublishedIndex];
 
     this.nextPost = nextPost;
