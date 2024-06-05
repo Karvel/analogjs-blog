a561f501eec26726174d4d57915f937daed4f457
Elanna Grossman <elanna.grossman@gmail.com>
2024-06-04 17:31:35 -0700

    refactor(header): switch from template driven to reactive form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,17 @@
 import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 import { siteName } from '@constants/site-name';
 import { Navigation } from '@models/navigation';
 import { ThemeService } from '@services/theme.service';
+import { distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AsyncPipe, FormsModule, NgClass, NgIf, NgFor, RouterLink],
+  imports: [AsyncPipe, NgClass, NgIf, NgFor, ReactiveFormsModule, RouterLink],
   styleUrls: ['./header.component.scss'],
   template: `
     <nav class="bg-[#829ab3] border-gray-300 dark:bg-sky-800">
@@ -72,39 +73,43 @@ import { ThemeService } from '@services/theme.service';
               >
             </li>
             <li>
-              <label
-                class="switch"
-                for="theme-toggle"
-                [attr.aria-label]="'Toggle theme'"
-                tabindex="0"
-                (keypress)="toggleTheme()"
-              >
-                <span class="sun"
-                  ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <circle r="5" cy="12" cx="12" />
-                    <path
-                      d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"
-                    /></svg
-                ></span>
-                <span class="moon"
-                  ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 384 512"
-                  >
-                    <path
-                      d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"
-                    /></svg
-                ></span>
-                <input
-                  class="input"
-                  id="theme-toggle"
-                  tabindex="-1"
-                  type="checkbox"
-                  (change)="toggleTheme()"
-                  [(ngModel)]="isChecked"
-                />
-                <span class="slider"></span>
-              </label>
+              <form [formGroup]="form">
+                <label
+                  class="switch"
+                  for="theme-toggle"
+                  [attr.aria-label]="'Toggle theme'"
+                  tabindex="0"
+                  (keypress)="toggleTheme()"
+                >
+                  <span class="sun"
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle r="5" cy="12" cx="12" />
+                      <path
+                        d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"
+                      /></svg
+                  ></span>
+                  <span class="moon"
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 384 512"
+                    >
+                      <path
+                        d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"
+                      /></svg
+                  ></span>
+                  <input
+                    class="input"
+                    id="theme-toggle"
+                    tabindex="-1"
+                    type="checkbox"
+                    formControlName="isChecked"
+                  />
+                  <span class="slider"></span>
+                </label>
+              </form>
             </li>
           </ul>
         </div>
@@ -112,8 +117,8 @@ import { ThemeService } from '@services/theme.service';
     </nav>
   `,
 })
-export default class HeaderComponent {
-  public isChecked!: boolean;
+export default class HeaderComponent implements OnInit {
+  public form: FormGroup = new FormGroup({});
   public linkList: Navigation[] = [
     {
       label: 'About',
@@ -139,9 +144,20 @@ export default class HeaderComponent {
   public theme$ = this.themeService.theme$;
 
   constructor() {
-    this.theme$.subscribe((theme) => {
-      this.isChecked = theme === 'light';
+    this.form = new FormGroup({
+      isChecked: new FormControl(false),
     });
+
+    this.theme$.subscribe((theme) =>
+      this.form.setValue({ isChecked: theme === 'light' }),
+    );
+  }
+
+  ngOnInit(): void {
+    this.form
+      ?.get('isChecked')
+      ?.valueChanges.pipe(distinctUntilChanged())
+      .subscribe(() => this.toggleTheme());
   }
 
   public toggleNavbar(): void {
