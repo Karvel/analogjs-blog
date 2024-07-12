7622dad4dcd4b401502530e1f0e3ca3d8fe89103
Elanna Grossman <elanna.grossman@gmail.com>
2024-07-11 17:46:40 -0700

    fix(header): set logo container height

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -21,12 +21,14 @@ import { ThemeService } from '@services/theme.service';
       >
         <div class="flex">
           <a routerLink="/" class="flex items-center no-underline">
-            <img
-              src="images/self/logo.png"
-              width="50"
-              alt="Hapax Legomenon logo"
-              loading="lazy"
-            />
+            <div class="h-[38px]">
+              <img
+                src="images/self/logo.png"
+                width="50"
+                alt="Hapax Legomenon logo"
+                loading="lazy"
+              />
+            </div>
             <span
               class="self-center text-lg sm:text-2xl font-semibold whitespace-nowrap dark:text-white max-[350px]:hidden"
               >{{ siteName }}</span
