0f2300d6964b807c535b92a3490220234ae2ef99
Elanna Grossman <elanna.grossman@gmail.com>
2024-07-11 20:34:36 -0700

    docs, test(app): housekeeping (#216)
    * docs(README): remove branch specific badges until reimplemented
    * test(app): remove deprecated test dependency
    * test(footer): restore footer tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
-import { provideLocationMocks } from '@angular/common/testing';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +7,7 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [provideRouter([]), provideLocationMocks()],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
