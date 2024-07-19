4244edf5e26bdcb04ab38e0a4f96c636fb97d7cf
Elanna Grossman <elanna.grossman@gmail.com>
2024-07-19 15:16:22 -0700

    test(paginator): add tests

--- src/app/components/archive/archive.component.spec.ts
+++ src/app/components/archive/archive.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
 
 import { ArchiveComponent } from './archive.component';
 
@@ -10,7 +9,6 @@ describe('ArchiveComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ArchiveComponent],
-      providers: [provideRouter([])],
     }).compileComponents();
   });
 
