8f8e480edd8549e5f7e5fa09035fe893c269996d
Elanna Grossman <elanna.grossman@gmail.com>
2024-07-18 18:58:29 -0700

    refactor(pipes): clean up `HighlightPipe` tests

--- src/app/pipes/highlight.pipe.spec.ts
+++ src/app/pipes/highlight.pipe.spec.ts
@@ -5,35 +5,40 @@ describe('[Unit] HighlightPipe', () => {
   it('should return a transformed string with each instance of the search value wrapped in <strong>', () => {
     const testValue: string = 'test';
     const searchValue: string = 't';
+    const actualValue = pipe.transform(testValue, searchValue);
     const expectedValue: string = '<strong>t</strong>es<strong>t</strong>';
-    expect(pipe.transform(testValue, searchValue)).toEqual(expectedValue);
+    expect(actualValue).toEqual(expectedValue);
   });
 
   it('should return a transformed string with the search value wrapped in <strong>', () => {
     const testValue: string = 'test data';
     const searchValue: string = 'te';
+    const actualValue = pipe.transform(testValue, searchValue);
     const expectedValue: string = '<strong>te</strong>st data';
-    expect(pipe.transform(testValue, searchValue)).toEqual(expectedValue);
+    expect(actualValue).toEqual(expectedValue);
   });
 
   it('should return the original value when the test data is not found', () => {
     const testValue: string = 'test data';
     const searchValue: string = 'x';
+    const actualValue = pipe.transform(testValue, searchValue);
     const expectedValue: string = 'test data';
-    expect(pipe.transform(testValue, searchValue)).toEqual(expectedValue);
+    expect(actualValue).toEqual(expectedValue);
   });
 
   it('should return the original value when there is no search value', () => {
     const testValue: string = 'test data';
     const searchValue: string = '';
+    const actualValue = pipe.transform(testValue, searchValue);
     const expectedValue: string = 'test data';
-    expect(pipe.transform(testValue, searchValue)).toEqual(expectedValue);
+    expect(actualValue).toEqual(expectedValue);
   });
 
   it('should return the original value when there is no test value or search value', () => {
     const testValue: string = '';
     const searchValue: string = '';
+    const actualValue = pipe.transform(testValue, searchValue);
     const expectedValue: string = '';
-    expect(pipe.transform(testValue, searchValue)).toEqual(expectedValue);
+    expect(actualValue).toEqual(expectedValue);
   });
 });
