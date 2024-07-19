af1f598b5af69f15411c6ed84a6bddbc1d907277
Elanna Grossman <elanna.grossman@gmail.com>
2024-07-19 14:58:56 -0700

    test(pages): add tests

---
+++ src/app/pages/[...page-not-found].page.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
+import PageNotFoundComponent from './[...page-not-found].page';
+
+describe('PageNotFoundComponent', () => {
+  let component: PageNotFoundComponent;
+  let fixture: ComponentFixture<PageNotFoundComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PageNotFoundComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageNotFoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
