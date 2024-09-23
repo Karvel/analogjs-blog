98b0c95d664fc53474a4e90cfdc96c9a226ec065
Elanna Grossman <elanna.grossman@gmail.com>
2024-09-22 19:34:14 -0700

    feat(photos): simplify loading flow for profile only data

--- src/app/pages/photos/index.page.ts
+++ src/app/pages/photos/index.page.ts
@@ -1,12 +1,10 @@
-import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { MetaDefinition } from '@angular/platform-browser';
 
 import { RouteMeta } from '@analogjs/router';
-import { catchError, of, tap } from 'rxjs';
 
 import { MasonryGridComponent } from '@components/masonry-grid/masonry-grid.component';
-import { SpinnerComponent } from '@components/spinner/spinner.component';
 import { siteName } from '@constants/site-name';
 import { FlickrService } from '@services/api/flickr.service';
 import { MetadataService } from '@services/metadata.service';
@@ -42,7 +40,7 @@ export const metaTagList: MetaDefinition[] = [
 @Component({
   selector: 'app-photos-index',
   standalone: true,
-  imports: [AsyncPipe, NgIf, MasonryGridComponent, SpinnerComponent, JsonPipe],
+  imports: [AsyncPipe, NgIf, MasonryGridComponent],
   template: `
     <div class="md:max-w md:mx-auto md:flex md:flex-col md:items-center">
       <div class="md:w-[48rem] p-4">
@@ -57,7 +55,6 @@ export const metaTagList: MetaDefinition[] = [
               >Flickr</a
             >.
           </div>
-          <app-spinner *ngIf="loading()" class="py-3 block" />
           <div *ngIf="profile$ | async as profile">
             <div
               *ngIf="profile?.description?._content"
@@ -77,14 +74,7 @@ export default class IndexPageComponent {
   private metadataService = inject(MetadataService);
 
   public loading: WritableSignal<boolean> = signal(true);
-  public profile$ = this.flickrService.getProfile().pipe(
-    tap(() => this.loading.set(false)),
-    catchError((error) => {
-      this.loading.set(false);
-
-      return of(error);
-    }),
-  );
+  public profile$ = this.flickrService.getProfile();
 
   constructor() {
     this.metadataService.setPageURLMetaTitle(pageTitle.title);
