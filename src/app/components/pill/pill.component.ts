efca37576bad7b428bc35bfaab12faa65bc95cf8
Elanna Grossman <elanna.grossman@gmail.com>
2024-09-26 01:29:54 -0700

    feat(pill): wire up `getColors()` to component

--- src/app/components/pill/pill.component.ts
+++ src/app/components/pill/pill.component.ts
@@ -1,23 +1,44 @@
-import { TitleCasePipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { NgClass, TitleCasePipe } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
 import { Category } from '@constants/category';
+import { Theme } from '@models/theme';
+import { getColors } from '@utils/get-colors';
 
 @Component({
   selector: 'app-pill',
   standalone: true,
-  imports: [RouterLink, TitleCasePipe],
+  imports: [NgClass, RouterLink, TitleCasePipe],
   template: `
     <a
-      class="my-1 py-1 px-3 no-underline rounded-full inline-block bg-neutral-500 dark:bg-neutral-700 text-white font-semibold text-xs hover:text-white dark:hover:text-white hover:bg-neutral-700 dark:hover:bg-neutral-500"
+      class="my-1 py-1 px-3 no-underline rounded-full inline-block font-semibold text-xs"
+      [ngClass]="[
+        'dark:bg-' + color.dark,
+        'bg-' + color.light,
+        'dark:hover:bg-' + color.light,
+        'hover:bg-' + color.dark,
+        color.text ? 'dark:text-' + color.text : 'dark:text-' + color.light,
+        color.text ? 'text-' + color.text : 'text-' + color.dark,
+        color.text
+          ? 'dark:hover:text-' + color.text
+          : 'dark:hover:text-' + color.dark,
+        color.text ? 'hover:text-' + color.text : 'hover:text-' + color.light
+      ]"
       [routerLink]="[route, slug]"
     >
       {{ label | titlecase }}
     </a>
   `,
 })
-export class PillComponent {
+export class PillComponent implements OnInit {
   @Input() public label!: Category | undefined;
   @Input() public route!: string | undefined;
   @Input() public slug!: string | undefined;
+
+  public color!: Theme;
+
+  public ngOnInit(): void {
+    this.color = getColors(this.label);
+  }
 }
