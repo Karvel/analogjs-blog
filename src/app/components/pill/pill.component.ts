06485e4267ba666f12956393611bf60141d30a11
Elanna Grossman <elanna.grossman@gmail.com>
2025-05-22 03:52:59 -0700

    fix, refactor(app): simplify color class setter

--- src/app/components/pill/pill.component.ts
+++ src/app/components/pill/pill.component.ts
@@ -1,5 +1,5 @@
 import { NgClass, TitleCasePipe } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { Theme } from '@models/theme';
@@ -12,32 +12,23 @@ import { getColors } from '@utils/get-colors';
   template: `
     <a
       class="my-1 py-1 px-3 no-underline rounded-full inline-block font-semibold text-xs"
-      [ngClass]="[
-        'dark:bg-' + color.dark,
-        'bg-' + color.light,
-        'dark:hover:bg-' + color.light,
-        'hover:bg-' + color.dark,
-        color.text ? 'dark:text-' + color.text : 'dark:text-' + color.light,
-        color.text ? 'text-' + color.text : 'text-' + color.dark,
-        color.text
-          ? 'dark:hover:text-' + color.text
-          : 'dark:hover:text-' + color.dark,
-        color.text ? 'hover:text-' + color.text : 'hover:text-' + color.light
-      ]"
+      [ngClass]="getColors2()"
       [routerLink]="[route, slug]"
     >
       {{ label | titlecase }}
     </a>
   `,
 })
-export class PillComponent implements OnInit {
+export class PillComponent {
   @Input() public label!: string | undefined;
   @Input() public route!: string | undefined;
   @Input() public slug!: string | undefined;
 
   public color!: Theme;
 
-  public ngOnInit(): void {
-    this.color = getColors(this.label);
+
+  public getColors2() {
+    return getColors(this.label);
   }
+
 }
--- src/app/utils/get-colors.ts
+++ src/app/utils/get-colors.ts
@@ -1,31 +1,17 @@
 import { Category } from '@constants/category';
-import { Theme } from '@models/theme';
 import { getCategoryFromLabel } from './get-category-from-label';
 
-export const getColors = (label: Category | string | undefined): Theme => {
+export const getColors = (label: Category | string | undefined): string => {
   const category = getCategoryFromLabel(label);
   switch (category) {
     case 'development':
-      return {
-        dark: 'blue-900',
-        light: 'blue-200',
-      };
+      return 'dark:bg-blue-900 bg-blue-200 dark:hover:bg-blue-200 hover:bg-blue-900 dark:text-blue-200 text-blue-900 dark:hover:text-blue-900 hover:text-blue-200';
     case 'photography':
-      return {
-        dark: 'emerald-900',
-        light: 'emerald-200',
-      };
+      return 'dark:bg-emerald-900 bg-emerald-200 dark:hover:bg-emerald-200 hover:bg-emerald-900 dark:text-emerald-200 text-emerald-900 dark:hover:text-emerald-900 hover:text-emerald-200';
     case 'miscellaneous':
-      return {
-        dark: 'rose-900',
-        light: 'rose-200',
-      };
+      return 'dark:bg-rose-900 bg-rose-200 dark:hover:bg-rose-200 hover:bg-rose-900 dark:text-rose-200 text-rose-900 dark:hover:text-rose-900 hover:text-rose-200';
     case '':
     default:
-      return {
-        dark: 'neutral-700',
-        light: 'neutral-500',
-        text: 'white',
-      };
+      return 'dark:bg-neutral-700 bg-neutral-500 dark:hover:bg-neutral-500 hover:bg-neutral-700 dark:text-white text-white dark:hover:text-white hover:text-white';
   }
 };
