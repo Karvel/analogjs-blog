6ba3961c1ffdeece8768c1da652bd1a56a4749a2
Elanna Grossman <elanna.grossman@gmail.com>
2025-08-08 21:16:46 -0700

    feat, refactor, style(app): update line endings, apply other updates

--- src/app/components/photo-album/photo-album.component.ts
+++ src/app/components/photo-album/photo-album.component.ts
@@ -1,14 +1,13 @@
 import { NgIf, NgOptimizedImage, NgStyle } from '@angular/common';
 import { Component, Input, signal, WritableSignal } from '@angular/core';
 
-import { SkeletonCardComponent } from '@components/skeleton-card/skeleton-card.component';
+import SkeletonCardComponent from '@components/skeleton-card/skeleton-card.component';
 import { flickr } from '@constants/flickr';
 import { ReplaceBrokenImageDirective } from '@directives/replace-broken-image.directive';
 import { PhotosetListItem } from '@models/flickr';
 
 @Component({
   selector: 'app-photo-album',
-  standalone: true,
   imports: [
     NgIf,
     NgOptimizedImage,
@@ -51,7 +50,7 @@ import { PhotosetListItem } from '@models/flickr';
         <div
           class="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-end p-4"
         >
-          <h2 class="text-white text-xl font-bold text-shadow-sm shadow-black">
+          <h2 class="text-white text-xl font-bold text-shadow-xs shadow-black">
             {{ photo.title._content }}
           </h2>
         </div>
@@ -59,7 +58,7 @@ import { PhotosetListItem } from '@models/flickr';
     </div>
   `,
 })
-export class PhotoAlbumComponent {
+export default class PhotoAlbumComponent {
   @Input() public photo!: PhotosetListItem;
 
   public flickr = flickr;
