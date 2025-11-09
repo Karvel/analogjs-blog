843b3fcb7c94f319d7e7f84463599276b00c78bd
Elanna Grossman <elanna.grossman@gmail.com>
2025-11-08 16:46:03 -0800

    refactor(popover): remove dead code

--- src/app/components/popover/image-info-popover-content.component.ts
+++ src/app/components/popover/image-info-popover-content.component.ts
@@ -1,13 +1,11 @@
-
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image-info-popover-content',
-  imports: [],
   template: `
     <div
       class="absolute right-4 bottom-12 bg-white dark:bg-[#242424] rounded-md p-4 text-slate-900 dark:text-neutral-100 border-2 dark:border-white border-slate-900"
-      >
+    >
       <a [href]="cover_image_source" class="" target="_blank" rel="noopener">{{
         cover_image_title
       }}</a>
@@ -15,7 +13,7 @@ import { Component, Input } from '@angular/core';
         <span> by {{ cover_image_author }}</span>
       }
     </div>
-    `,
+  `,
 })
 export default class ImageInfoPopoverContentComponent {
   @Input() public cover_image_author!: string | undefined;
