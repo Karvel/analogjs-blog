d4712975c59beca1d2a37e961ea493a41658f2d1
Elanna Grossman <elanna.grossman@gmail.com>
2025-11-10 21:45:44 -0800

    test(utils): clean up tests

--- src/app/utils/sort-by-date.spec.ts
+++ src/app/utils/sort-by-date.spec.ts
@@ -5,86 +5,132 @@ import { sortByDate } from './sort-by-date';
 
 describe('sortByDate', () => {
   it('should consider both null or undefined parameters as equal', () => {
-    const result = sortByDate(null, undefined);
-    expect(result).toBe(0); // Both are null or undefined, should be considered equal
+    const actualValue = sortByDate(null, undefined);
+    // Both are null or undefined, should be considered equal
+    const expectedValue = 0;
+    expect(actualValue).toBe(expectedValue);
   });
 
-  it('should consider null or undefined "a" as "b" comes first', () => {
+  it('should sort "b" before "a" when "a" is null or undefined', () => {
     const fileA: ContentFile<BlogPost> | null | undefined = null;
     const fileB: ContentFile<BlogPost> = {
       attributes: {
-        last_updated: '2023-09-15T00:28:38-07:00',
         date: '2023-09-10T00:28:38-07:00',
       },
       filename: 'test2.md',
       slug: 'test2',
     };
-    const result = sortByDate(fileA, fileB);
-    expect(result).toBe(1); // a is null or undefined, b comes first
+    const actualValue = sortByDate(fileA, fileB);
+    // a is null or undefined, b comes first
+    const expectedValue = 1;
+    expect(actualValue).toBe(expectedValue);
   });
 
-  it('should consider null or undefined "b" as "a" comes first', () => {
+  it('should sort "a" before "b" when "b" is null or undefined', () => {
     const fileA: ContentFile<BlogPost> = {
       attributes: {
-        last_updated: '2023-09-15T00:28:38-07:00',
         date: '2023-09-10T00:28:38-07:00',
       },
       filename: 'test1.md',
       slug: 'test1',
     };
     const fileB: ContentFile<BlogPost> | null | undefined = null;
-    const result = sortByDate(fileA, fileB);
-    expect(result).toBe(-1); // b is null or undefined, a comes first
+    const actualValue = sortByDate(fileA, fileB);
+    // b is null or undefined, a comes first
+    const expectedValue = -1;
+    expect(actualValue).toBe(expectedValue);
   });
 
-  it('should correctly sort by date when both dates are present', () => {
-    const file1: ContentFile<BlogPost> = {
+  it('should correctly sort by date when both dates are present and a is greater than b', () => {
+    const fileA: ContentFile<BlogPost> = {
       attributes: {
-        last_updated: '2023-09-12T00:28:38-07:00',
         date: '2023-09-14T00:28:38-07:00',
       },
       filename: 'test1.md',
       slug: 'test1',
     };
-    const file2: ContentFile<BlogPost> = {
+    const fileB: ContentFile<BlogPost> = {
       attributes: {
-        last_updated: '2023-09-15T00:28:38-07:00',
         date: '2023-09-10T00:28:38-07:00',
       },
       filename: 'test2.md',
       slug: 'test2',
     };
-    const result = sortByDate(file1, file2);
-    expect(result).toBeGreaterThan(0); // file2 should come before file1
+    const actualValue = sortByDate(fileA, fileB);
+    // fileA should come before fileB
+    const expectedValue = 0;
+    expect(actualValue).toBeLessThan(expectedValue);
+  });
+
+  it('should correctly sort by date when both dates are present and b is greater than a', () => {
+    const fileA: ContentFile<BlogPost> = {
+      attributes: {
+        date: '2023-09-10T00:28:38-07:00',
+      },
+      filename: 'test1.md',
+      slug: 'test1',
+    };
+    const fileB: ContentFile<BlogPost> = {
+      attributes: {
+        date: '2023-09-14T00:28:38-07:00',
+      },
+      filename: 'test2.md',
+      slug: 'test2',
+    };
+    const actualValue = sortByDate(fileA, fileB);
+    // fileB should come before fileA
+    const expectedValue = 0;
+    expect(actualValue).toBeGreaterThan(expectedValue);
   });
 
   it('should correctly sort by date when only one date is present', () => {
-    const file1: ContentFile<BlogPost> = {
+    const fileA: ContentFile<BlogPost> = {
       attributes: { date: '2023-09-10T00:28:38-07:00' },
       filename: 'test1.md',
       slug: 'test1',
     };
-    const file2: ContentFile<BlogPost> = {
-      attributes: { last_updated: '2023-09-12T00:28:38-07:00' },
+    const fileB: ContentFile<BlogPost> = {
+      attributes: {},
       filename: 'test2.md',
       slug: 'test2',
     };
-    const result = sortByDate(file1, file2);
-    expect(result).toBeGreaterThan(0); // file2 should come before file1
+    const actualValue = sortByDate(fileA, fileB);
+    // b is null or undefined, a comes first
+    const expectedValue = -1;
+    expect(actualValue).toBe(expectedValue);
   });
 
-  it('should return 0 when both files have no date information', () => {
-    const file1: ContentFile<BlogPost> = {
+  it('should correctly sort by date when only one date is present', () => {
+    const fileA: ContentFile<BlogPost> = {
       attributes: {},
       filename: 'test1.md',
       slug: 'test1',
     };
-    const file2: ContentFile<BlogPost> = {
+    const fileB: ContentFile<BlogPost> = {
+      attributes: { date: '2023-09-10T00:28:38-07:00' },
+      filename: 'test2.md',
+      slug: 'test2',
+    };
+    const actualValue = sortByDate(fileA, fileB);
+    // a is null or undefined, b comes first
+    const expectedValue = 1;
+    expect(actualValue).toBe(expectedValue);
+  });
+
+  it('should return 0 when both files have no date information', () => {
+    const fileA: ContentFile<BlogPost> = {
+      attributes: {},
+      filename: 'test1.md',
+      slug: 'test1',
+    };
+    const fileB: ContentFile<BlogPost> = {
       attributes: {},
       filename: 'test2.md',
       slug: 'test2',
     };
-    const result = sortByDate(file1, file2);
-    expect(result).toBe(0); // Both files have no date information, so they should be considered equal
+    const actualValue = sortByDate(fileA, fileB);
+    // Both are null or undefined, should be considered equal
+    const expectedValue = 0;
+    expect(actualValue).toBe(expectedValue);
   });
 });
